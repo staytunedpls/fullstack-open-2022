91eaa2e926e6048aabd9ab78b0dff4363bf5cebc
Sergey Starikov <sergeystarikov96@gmail.com>
2022-07-21 23:19:43 +0400

    Now 1.6 is really done

--- part_1/1.6/src/App.js
+++ part_1/1.6/src/App.js
@@ -1,12 +1,35 @@
 import { useState } from "react";
 
+const Header = () => <h1>give feedback</h1>;
+const Statistics = () => <h1>statistics</h1>;
+const StatsLine = (props) => (
+  <p>
+    {props.name} {props.value}
+  </p>
+);
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <Header />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Statistics />
+      <StatsLine name="good" value={good} />
+      <StatsLine name="neutral" value={neutral} />
+      <StatsLine name="bad" value={bad} />
+    </div>
+  );
 };
 
 export default App;
