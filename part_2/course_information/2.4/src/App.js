7ba2d8e6d6b4687732a3766c284ed0e01fcb1e6f
Sergey Starikov <sergeystarikov96@gmail.com>
2022-07-23 00:23:24 +0400

    Done 2.4

---
+++ part_2/course_information/2.4/src/App.js
@@ -0,0 +1,97 @@
+const Header = (props) => {
+  return <h2>{props.course}</h2>;
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return parts.map((part) => (
+    <Part key={part.id} name={part.name} exercises={part.exercises} />
+  ));
+};
+
+const Total = ({ parts }) => {
+  const total_exercises = parts.reduce(
+    (result, part) => result + part.exercises,
+    0
+  );
+  return (
+    <p>
+      <strong>total of {total_exercises} exercises</strong>
+    </p>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  );
+};
+
+const App = () => {
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
+
+  return (
+    <>
+      <h1>Web development curriculum</h1>
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
+    </>
+  );
+};
+
+export default App;
